9fc0931c1d3c76650f5391bbe57038a31dbd189b
Mateus Luiz Gonzaga <mateusluizgonzaga@gmail.com>
2023-06-26 17:52:33 -0300

    criando rotas do to do list

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 const express = require('express');
+const checklistRouter = require('./src/routes/checklist')
+const app = express();
+
+app.use(express.json()); //middleware do Express para processar  JSON quando for passado um JSON no Body da requisição
+app.use('/checklists',checklistRouter)
+
+
+app.listen(3000,()=> {
+    console.log('Servidor ativo.')
+})
