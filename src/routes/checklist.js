aa35b533e7026e2192f39b6b78e5134ccf7fc435
“mlgonzaga” <mateusluizgonzaga@gmail.com>
2023-07-09 18:20:58 -0300

    adding routes PUT and DELETE

--- src/routes/checklist.js
+++ src/routes/checklist.js
@@ -13,8 +13,21 @@ router.post('/', (req, res) => {
 })
 
 router.get("/:id", (req, res) =>{
-    console.log(req.params.id)
+    console.log(req.body)
     res.send(`ID: ${req.params.id}`)
 })
+
+router.put("/:id", (req, res) =>{
+    console.log(req.params.id)
+    res.send(`PUT ID: ${req.params.id}`)
+})
+
+router.delete("/:id", (req, res) =>{
+    console.log(req.params.id)
+    res.send(`DELETE ID: ${req.params.id}`)
+})
+
+
+
 
 module.exports = router
